c4e5dc3699ba9fa2779a3e3855e5233fe3fa0ebf
Satwik Tiwari <102408465+satwiktiwari22@users.noreply.github.com>
2023-09-11 12:20:29 +0530

    Create exp4.cpp

---
+++ exp4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number Accepted"<<endl;
+    }
+    else{
+        cout<<"Number Rejected"<<endl;
+    }
+
+    return 0;
+}
